c3a250541768f129eb77bb3280e1764285ff78ea
Rizwan <rizwanrensa@gmail.com>
2019-10-02 20:33:21 -0400

    created the Article model

---
+++ models/Article.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+const ArticleSchema = new Schema({
+  // `title` is required and of type String
+  headline: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  URL: {
+    type: String,
+    required: true
+  },
+
+  // `note` is an object that stores a Note id
+  // The ref property links the ObjectId to the Note model
+  // This allows us to populate the Article with an associated Note
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+
+
+})
+
+const Article = mongoose.model("Article", ArticleSchema)
+
+module.exports = Article;
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 // Require all models
 //var db = require("./models");
 
 
-var PORT = 3000;
+const PORT = 3000;
 
 // Initialize Express
-var app = express();
+const app = express();
 
 
 // Configure middleware
